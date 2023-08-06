3448023ec76da1e72eb792703df149ef9024f3b4
Nandhana2022 <nandhunarendran2000@gmail.com>
2023-08-06 12:49:02 +0530

    Initial commit

---
+++ TodoAppBackend/TodoAppBackend/Data/TodoDbContext.cs
@@ -0,0 +1,15 @@
+﻿// TodoDbContext.cs
+using Microsoft.EntityFrameworkCore;
+using TodoAppBackend.Models;
+
+namespace TodoAppBackend.Data
+{
+    public class TodoDbContext : DbContext
+    {
+        public TodoDbContext(DbContextOptions<TodoDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
