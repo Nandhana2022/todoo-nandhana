9b628ad95d4ead1cd1cfc429ab313c105e117d2f
Nandhana2022 <122627080+Nandhana2022@users.noreply.github.com>
2023-08-06 13:28:26 +0530

    Add files via upload

---
+++ TodoAppBackend/Models/TodoItem.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoAppBackend.Models
+{
+    public class TodoItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Description { get; set; }
+
+        public bool Completed { get; set; }
+    }
+}
+
+
